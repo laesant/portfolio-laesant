780bf3917e0b1e6c581b023e8c1ea547d6741dca
laesant <laenio.knot@outlook.com>
2023-01-08 21:40:44 -0300

    changes

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -2,9 +2,34 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+projects = [
+    {
+        "name": "Aplicativo de rastreamento de hábitos com Python e MongoDB",
+        "thumb": "img/habit-tracking.png",
+        "hero": "img/habit-tracking-hero.png",
+        "categories": ["python", "web"],
+        "slug": "habit-tracker",
+        "prod": "https://habit-tracker-wvvz.onrender.com"
+    },
+    {
+        "name": "Aplicativo de rastreamento de finanças pessoais com React",
+        "thumb": "img/personal-finance.png",
+        "hero": "img/personal-finance.png",
+        "categories": ["react", "javascript"],
+        "slug": "personal-finance",
+    },
+    {
+        "name": "Documentação da API REST com Postman e Swagger",
+        "thumb": "img/rest-api-docs.png",
+        "hero": "img/rest-api-docs.png",
+        "categories": ["writing"],
+        "slug": "api-docs",
+    }
+]
+
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", projects=projects)
 
 @app.route("/about")
 def about():
@@ -12,4 +37,4 @@ def about():
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.jinja2")
